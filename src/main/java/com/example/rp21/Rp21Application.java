7b5b6423fdae1552e5bc09d9bd44e795f7cac894
Georg_Allas <55129274+georgha123@users.noreply.github.com>
2021-09-16 19:01:15 +0300

    First homework

---
+++ src/main/java/com/example/rp21/Rp21Application.java
@@ -0,0 +1,13 @@
+package com.example.rp21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rp21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rp21Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rp21/Rp21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rp21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rp21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
