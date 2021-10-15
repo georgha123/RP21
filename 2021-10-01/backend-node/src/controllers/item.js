faec51ccb7edd91266dbfb90c6dd39e855e56b81
Georg-Henri Allas <georgha@tlu.ee>
2021-10-15 14:28:07 +0300

    15.01.21 homework

---
+++ 2021-10-01/backend-node/src/controllers/item.js
@@ -0,0 +1,43 @@
+const Item = require('../models/Item')
+
+exports.getItems = async (req, res) => {
+  const items = await Item.find({})
+  
+  res.status(200).send(items)
+}
+
+exports.createItem = async (req, res) => {
+  const newItem = {
+    name: "Table",
+    quality: 50,
+    unused: true,
+    color: "blue"
+  }
+
+  const createdItem = new Item(newItem)
+
+  const savedItem = await createdItem.save()
+
+  res.status(200).send(`yay ${savedItem._id}`)
+}
+
+exports.updateItem = async (req, res) => {
+  const { id } = req.params;
+
+  const items = await Item.findById(id)
+
+  const item = await Item.findOneAndUpdate( { _id: id }, { quality: items.quality+1 })
+
+  res.status(200).send(`updated ${item._id}`) 
+
+}
+
+exports.deleteItem = async (req, res) => {
+  const { id } = req.params;
+
+  const item = await Item.findOneAndDelete({ _id: id })
+
+  if (!item) res.status(404).send("No item with that id found")
+
+  res.status(200).send(`Successfully deleted the following item: \n ${item}`)
+}
